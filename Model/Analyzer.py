395b995f62e7c2bd8dd8a3caa95c4a8296873793
wenbinxu <wenbinxu@fhi.mpg.de>
2021-01-31 23:06:21 +0100

    20200131

---
+++ Model/Analyzer.py
@@ -0,0 +1,69 @@
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+import numpy as np
+from ML_learning import Train_gpr, Train_krr
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from statistics import mean
+
+def plt_distribution(x, n_bin, dpi=100):
+    fig, axs = plt.subplots(tight_layout=True)
+    #fig, axs = plt.subplots(1, 2, tight_layout=True) #sharey=True
+    axs.hist(x, bins=n_bin)
+    axs.set_xlabel('Adsorption energy (eV)')
+    axs.set_ylabel('Numbers')
+    # axs[1].hist(x, bins=n_bin, density=True)
+    # axs[1].yaxis.set_major_formatter(PercentFormatter(xmax=1))
+    plt.savefig('distribution.png',dpi=dpi)
+
+def Check_outlier(target,prediction,name_list,threshold=0.5):
+    outlier_list = {}
+    for i, values in enumerate(list(zip(target,prediction,name_list))):
+        abs_error = abs(values[0]-values[1])
+        if abs_error > threshold:
+            #extract file name of outlier to a list
+            outlier_list[values[2]]=abs_error
+    return dict(sorted(outlier_list.items(), key=lambda item: item[1]))
+    
+
+def KF_validation(kernel_matrix, y, ML_method, name_list=None, 
+                  n_split=5,shuffle=True,random_state=0
+                  ):
+    kf = KFold(n_splits=n_split,shuffle=shuffle,random_state=random_state)
+    train_RMSEs,train_MAEs,test_RMSEs,test_MAEs,test_outliers =[],[],[],[],{}
+    for train_index, test_index in kf.split(kernel_matrix[0],y):
+        #get train_matrix and test_matrix
+        train_matrix = kernel_matrix[train_index][:,train_index]
+        test_matrix  = kernel_matrix[test_index][:,train_index]
+        #y_train, y_test, name_train, name_test
+        y_train, y_test = y[train_index], y[test_index]
+        name_train, name_test = name_list[train_index], name_list[test_index] #outlier
+        #train a model
+        if ML_method == 'gpr':
+            ml_model = Train_gpr(train_matrix, y_train)
+        if ML_method == 'krr':
+            ml_model = Train_krr(train_matrix, y_train)   
+        #predict training and validation
+        train_pre = ml_model.predict(train_matrix)
+        test_pre  = ml_model.predict(test_matrix)
+        test_outlier = Check_outlier(y_test,test_pre,name_test)
+        #test_outlier= Check_outlier(y_test,test_pre,name_test)
+        # evaluate MAE and RMSE
+        train_MAE = mean_absolute_error(train_pre, y_train)
+        test_MAE  = mean_absolute_error(test_pre, y_test)
+        train_RMSE = mean_squared_error(train_pre, y_train,squared=False)
+        test_RMSE  = mean_squared_error(test_pre, y_test,squared=False)
+        # append
+        train_MAEs.append(train_MAE)
+        train_RMSEs.append(train_RMSE)
+        test_MAEs.append(test_MAE)
+        test_RMSEs.append(test_RMSE)
+        test_outliers.update(test_outlier)
+        
+    avr_train_MAE = mean(train_MAEs)
+    avr_test_MAE = mean(test_MAEs)
+    avr_train_RMSE = mean(train_RMSEs)
+    avr_test_RMSE = mean(test_RMSEs)
+    return avr_train_MAE, avr_train_RMSE, avr_test_MAE, avr_test_RMSE,test_outliers
+
+
---
+++ Model/main.py
@@ -0,0 +1,149 @@
+
+#%%
+from wwl import wwl
+from Utility import CollectInputData,get_catkit_attribute
+from ase.io import read, write
+from Processdata import Collect_graph, Collect_node_attributes, CollectDatatoPandas, Collect_ads_energies
+import igraph as ig
+from Analyzer import KF_validation, plt_distribution
+
+
+database_path = 'tmp/database_Cu_761/'
+file_names  = CollectInputData(database_path)
+
+graphs = Collect_graph(database_path, cutoff_mult=1, weighted=False, skin=0.1)
+catkit_attr = get_catkit_attribute()  #catkit feature library 
+
+# all_feature_list = ['atomic_number', 
+#             'atomic_radius', 
+#             #'atomic_radius_rahm',
+#             'atomic_volume', 
+#             #'atomic_weight', 
+#             'boiling_point', 
+#             #'c6', 
+#             #'c6_gb', 
+#             #'covalent_radius_bragg', 
+#             #'covalent_radius_cordero', 
+#             #'covalent_radius_pyykko', 
+#             'covalent_radius_pyykko_double',
+#             #'covalent_radius_pyykko_triple',
+#             #'covalent_radius_slater', 
+#             'dband_center_bulk', 
+#             #'dband_center_slab', 
+#             'dband_kurtosis_bulk',
+#             #'dband_kurtosis_slab', 
+#             'dband_skewness_bulk', 
+#             #'dband_skewness_slab', 
+#             'dband_width_bulk', 
+#             #'dband_width_slab', 
+#             'density',
+#             'dipole_polarizability',
+#             'electron_affinity', 
+#             #'en_allen', 
+#             'en_ghosh',
+#             'en_pauling', 
+#             #'evaporation_heat',
+#             'fusion_heat', 
+#             #'gas_basicity', 
+#             'group_id', 
+#             'heat_of_formation',
+#             'lattice_constant', 
+#             #'melting_point', 
+#             #'metallic_radius', 
+#             #'metallic_radius_c12', 
+#             #'period',
+#             'proton_affinity', 
+#             'specific_heat', 
+#             'thermal_conductivity', 
+#             'vdw_radius', 
+#             #'vdw_radius_alvarez',
+#             #'vdw_radius_batsanov', 
+#             #'vdw_radius_bondi', 
+#             #'vdw_radius_dreiding', 
+#             #'vdw_radius_mm3', 
+#             'vdw_radius_rt', 
+#             #'vdw_radius_truhlar',
+#             'vdw_radius_uff']
+
+# import itertools
+# feature_combina = list(itertools.combinations(all_feature_list,8))
+# import random
+# train_RMSEs,train_MAEs,test_RMSEs,test_MAEs =[],[],[],[]
+# for  feature_list in random.sample(feature_combina,50):
+# feature_list = list(feature_list)
+
+
+#feature_list = ['atomic_number', 'vdw_radius','electron_affinity','en_pauling' , 'dband_center_bulk', 'dband_width_bulk']#,'Local_parameter']
+feature_list  = ['Local_parameter', 'vdw_radius', 'electron_affinity']
+# feature_list = ['atomic_number']
+# feature_list = ['PE', 'EA', 'IE', 'GP']
+#feature_list = ['atomic_number','atomic_volume','electron_affinity',
+                #'en_pauling' , 'dband_center_bulk', 'heat_of_formation', 'lattice_constant', 'dband_width_bulk']
+
+node_attributes = Collect_node_attributes(database_path, feature_list=feature_list, 
+                                        attr_pool=catkit_attr, normalize=True)
+ads_energies = Collect_ads_energies(database_path)
+datas = CollectDatatoPandas(graphs,ads_energies,file_names)
+
+    # plt_distribution(datas['target'], n_bin=20, dpi=100)
+    # train_RMSEs,train_MAEs,test_RMSEs,test_MAEs =[],[],[],[]
+    
+train_RMSEs,train_MAEs,test_RMSEs,test_MAEs =[],[],[],[]
+for iter in range(1,2):
+    kernel_matrix = wwl(graphs, node_features=node_attributes, num_iterations=iter, sinkhorn=False, gamma=None)
+    train_MAE, train_RMSE, test_MAE, test_RMSE, test_outliers = \
+    KF_validation(kernel_matrix=kernel_matrix, y=datas['target'], name_list=datas['name'], ML_method='gpr',
+                n_split=5,shuffle=True,random_state=0)
+    train_MAEs.append(train_MAE)
+    train_RMSEs.append(train_RMSE)
+    test_MAEs.append(test_MAE)
+    test_RMSEs.append(test_RMSE)
+    print(feature_list)
+    print(train_MAE)
+    print(train_RMSE)
+    print(test_MAE)
+    print(test_RMSE)
+
+# # %%
+# import matplotlib.pyplot as plt
+# import matplotlib.ticker as ticker
+# x=[i for i in range(50)]
+# fig, ax = plt.subplots()
+# ax.plot(x, train_MAEs, color='r' ,label='train MAE')
+# ax.plot(x, train_RMSEs, color='blue',label='train RMSE')
+# ax.plot(x, test_MAEs,'k--', color='r', label='test MAE')
+# ax.plot(x, test_RMSEs,'k--', color='blue', label='test RMSE')
+# ax.set_ylim(0, 0.5)
+# ax.set_ylabel('Error eV')
+# # ax.set_xlim(0,0.5)
+# ax.legend(loc='upper right', bbox_to_anchor=(1.3, 1),shadow=True)
+# ax.set_xlabel('numer of iteration')
+# ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+# # ax.set_xlabel('gamma value')
+# # ax.set_xticklabels(['1e-1', '1e-2','1e-4','1e-6','1e-8','1e-10','1e-15','1e-20'])
+# # ax.set_ylabel('MAE or RMSE error (eV)')
+# plt.savefig('ML_curve.png',dpi=500)
+
+
+
+#%%
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+x=[1,2,3]
+fig, ax = plt.subplots()
+ax.plot(x, train_MAEs, color='r' ,label='train MAE')
+ax.plot(x, train_RMSEs, color='blue',label='train RMSE')
+ax.plot(x, test_MAEs,'k--', color='r', label='test MAE')
+ax.plot(x, test_RMSEs,'k--', color='blue', label='test RMSE')
+ax.set_ylim(0, 0.5)
+ax.set_ylabel('Error eV')
+# ax.set_xlim(0,0.5)
+ax.legend(loc='upper right', bbox_to_anchor=(1.3, 1),shadow=True)
+ax.set_xlabel('numer of iteration')
+ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+# ax.set_xlabel('gamma value')
+# ax.set_xticklabels(['1e-1', '1e-2','1e-4','1e-6','1e-8','1e-10','1e-15','1e-20'])
+# ax.set_ylabel('MAE or RMSE error (eV)')
+plt.savefig('ML_curve.png',dpi=500)
+# %%

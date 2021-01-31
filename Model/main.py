bb45eb88b223639be41fc1d0a27dbec6bd9623a7
wenbinxu <wenbinxu@fhi.mpg.de>
2021-01-31 23:08:18 +0100

    0.2

--- Model/main.py
+++ Model/main.py
@@ -14,66 +14,7 @@ file_names  = CollectInputData(database_path)
 graphs = Collect_graph(database_path, cutoff_mult=1, weighted=False, skin=0.1)
 catkit_attr = get_catkit_attribute()  #catkit feature library 
 
-# all_feature_list = ['atomic_number', 
-#             'atomic_radius', 
-#             #'atomic_radius_rahm',
-#             'atomic_volume', 
-#             #'atomic_weight', 
-#             'boiling_point', 
-#             #'c6', 
-#             #'c6_gb', 
-#             #'covalent_radius_bragg', 
-#             #'covalent_radius_cordero', 
-#             #'covalent_radius_pyykko', 
-#             'covalent_radius_pyykko_double',
-#             #'covalent_radius_pyykko_triple',
-#             #'covalent_radius_slater', 
-#             'dband_center_bulk', 
-#             #'dband_center_slab', 
-#             'dband_kurtosis_bulk',
-#             #'dband_kurtosis_slab', 
-#             'dband_skewness_bulk', 
-#             #'dband_skewness_slab', 
-#             'dband_width_bulk', 
-#             #'dband_width_slab', 
-#             'density',
-#             'dipole_polarizability',
-#             'electron_affinity', 
-#             #'en_allen', 
-#             'en_ghosh',
-#             'en_pauling', 
-#             #'evaporation_heat',
-#             'fusion_heat', 
-#             #'gas_basicity', 
-#             'group_id', 
-#             'heat_of_formation',
-#             'lattice_constant', 
-#             #'melting_point', 
-#             #'metallic_radius', 
-#             #'metallic_radius_c12', 
-#             #'period',
-#             'proton_affinity', 
-#             'specific_heat', 
-#             'thermal_conductivity', 
-#             'vdw_radius', 
-#             #'vdw_radius_alvarez',
-#             #'vdw_radius_batsanov', 
-#             #'vdw_radius_bondi', 
-#             #'vdw_radius_dreiding', 
-#             #'vdw_radius_mm3', 
-#             'vdw_radius_rt', 
-#             #'vdw_radius_truhlar',
-#             'vdw_radius_uff']
 
-# import itertools
-# feature_combina = list(itertools.combinations(all_feature_list,8))
-# import random
-# train_RMSEs,train_MAEs,test_RMSEs,test_MAEs =[],[],[],[]
-# for  feature_list in random.sample(feature_combina,50):
-# feature_list = list(feature_list)
-
-
-#feature_list = ['atomic_number', 'vdw_radius','electron_affinity','en_pauling' , 'dband_center_bulk', 'dband_width_bulk']#,'Local_parameter']
 feature_list  = ['Local_parameter', 'vdw_radius', 'electron_affinity']
 # feature_list = ['atomic_number']
 # feature_list = ['PE', 'EA', 'IE', 'GP']
